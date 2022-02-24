b460aea3650535c5b34bc62eba1d687435d9ede8
Sergey Kadiyevskiy <51814805+Kalashnik0v1947@users.noreply.github.com>
2022-02-23 19:15:51 -0500

    first update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import foods from './foods.json';
+import React from 'react'
+
+const [food, setFood] = React.useState(foods)
+console.log(food)
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(<div>
+    
+  {food.map(()=>{
+    return (
+      <div>
+  <p>food</p>
+  <img src="FOOD_IMAGE_HERE" width={0} />
+</div>
+    )
+  })}
+
+</div>)
 }
 
 export default App;
